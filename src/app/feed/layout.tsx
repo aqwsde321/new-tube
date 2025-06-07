daeffbf9ac9f2db2ccb4bd035b753de191ccef15
qrqrq23r <qrqrq23r@gmail.com>
2025-06-07 16:30:40 +0900

    레이아웃 추가

---
+++ src/app/feed/layout.tsx
@@ -0,0 +1,18 @@
+interface LayoutProps {
+    children: React.ReactNode;
+};
+
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <div>
+            <div className="p-4 bg-rose-500 w-full">
+                i am a navbar
+            </div>
+            {children}
+        </div>
+    );
+
+}
+
+export default Layout; 
