af5b685ce9fe05f09c4e573db3e6a1428c0bcb7d
qrqrq23r <qrqrq23r@gmail.com>
2025-06-08 02:38:36 +0900

    trpc 서버컴포넌트로 사용

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,12 @@
-"use client";
+import { trpc } from "@/trpc/server";
 
-import { trpc } from "@/trpc/client";
-
-export default function Home() {
-  const { data } = trpc.hello.useQuery({ text: "Antonio2" });
+export default async function Home() {
+  const data = await trpc.hello({ text: "Antonio2" });
 
   return (
     <div>
       <p className="text-xl font-semibold tracking-tight">
-        Client component says: {data?.greeting}
+        Client component says: {data.greeting}
       </p>
     </div>
   );
