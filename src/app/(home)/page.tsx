9a454e70df232f957701412fbfe429785a6b64b9
qrqrq23r <qrqrq23r@gmail.com>
2025-06-08 02:36:22 +0900

    trpc 추가 / 클라이언트 사용

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
 
 export default function Home() {
+  const { data } = trpc.hello.useQuery({ text: "Antonio2" });
+
   return (
     <div>
-      <p className="text-xl font-semibold tracking-tight">I will load videos in the future!</p>
+      <p className="text-xl font-semibold tracking-tight">
+        Client component says: {data?.greeting}
+      </p>
     </div>
   );
 }
