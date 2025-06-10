9a37e5f9ec28d828ffe47ab14d9b4e011a0aa45d
qrqrq23r <qrqrq23r@gmail.com>
2025-06-10 22:50:50 +0900

    home 페이지 정리
    강사 작성 규칙
    1. 서버 데이터 캐싱하는 곳
     import { HydrateClient, trpc } from "@/trpc/server";
    HydrateClient 과 trpc 를 동일 컴포넌트에서 사용
    
    2. 클라이언트 캐싱된 데이터 사용하는 곳
    import { Suspense } from "react";
    import { ErrorBoundary } from "react-error-boundary";
    import { trpc } from "@/trpc/client";
    위 3개 동일 컴포넌트에 사용하기

---
+++ src/modules/home/ui/sections/category-section.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { trpc } from "@/trpc/client";
+
+interface CategoriesSectionProps {
+    categoryId?: string;
+}
+
+export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading categories...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+
+const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
+    const [categories] = trpc.categories.getMany.useSuspenseQuery();
+
+    return (
+        <div>
+            {JSON.stringify(categories)}
+        </div>
+    );
+}
