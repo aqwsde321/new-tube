3ca7cdf9961f0a3aa4c40c6fb928080347840397
qrqrq23r <qrqrq23r@gmail.com>
2025-06-14 19:25:43 +0900

    videos 테이블 추가
    bunx drizzle-kit push
    bunx drizzle-kit studio

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,19 +1,69 @@
-import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import { ca } from "date-fns/locale";
+import { relations } from "drizzle-orm";
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  clerkId: text("clerkId").notNull().unique(),
-  name: text("name").notNull(),
-  // TODO: add banner fields
-  imageUrl: text("imageUrl").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
-}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerkId").notNull().unique(),
+    name: text("name").notNull(),
+    // TODO: add banner fields
+    imageUrl: text("imageUrl").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
 
-export const categories = pgTable("categories", {
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const categories = pgTable(
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull().unique(),
+    description: text("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("name_idx").on(t.name)]
+);
+
+export const categoryRelations = relations(categories, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const videos = pgTable("videos", {
   id: uuid("id").primaryKey().defaultRandom(),
-  name: text("name").notNull().unique(),
+  title: text("title").notNull(),
   description: text("description"),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  categoryId: uuid("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
-}, (t) => [uniqueIndex("name_idx").on(t.name)]);
+});
+
+// Relations only application level, not database level
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+    category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}));
