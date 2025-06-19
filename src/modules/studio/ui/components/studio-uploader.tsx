76b2d1ed105d68a62c525b0c2bd0cad1695a0c20
qrqrq23r <qrqrq23r@gmail.com>
2025-06-19 21:11:22 +0900

    mux 업로드 모달 onSuccss 추가

--- src/modules/studio/ui/components/studio-uploader.tsx
+++ src/modules/studio/ui/components/studio-uploader.tsx
@@ -20,6 +20,7 @@ export const StudioUploader = ({
   return (
     <div>
       <MuxUploader
+        onSuccess={onSuccess}
         endpoint={endpoint}
         id={UPLOADER_ID}
         className="hidden group/uploader"
